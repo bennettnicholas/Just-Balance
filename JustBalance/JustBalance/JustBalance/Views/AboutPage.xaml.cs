be61c956b1153ea53bca104102493a6efcf8426c
bennettnicholas <nicholas.bennett@mines.sdsmt.edu>
2021-12-07 08:17:50 -0800

    base app

---
+++ JustBalance/JustBalance/JustBalance/Views/AboutPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace JustBalance.Views
+{
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
